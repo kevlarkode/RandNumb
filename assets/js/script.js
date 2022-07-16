866d5e0a840c0559d0bddf580fa9517d54e5a745
Tulsi Dey <deytulsi18@gmail.com>
2022-07-16 14:15:05 +0530

    restructed folders and added code improvements

---
+++ assets/js/script.js
@@ -0,0 +1,124 @@
+
+/******* Getting the initial seed value from page load time *******/
+
+let seed = Math.round(performance.now());
+const MAX_NUM = Number.MAX_SAFE_INTEGER;
+const maxLimitForNumberGeneration = 1e10;
+
+/******* Function for getting execution time of some code snippet *******/
+
+let doSomething = (limit) => {
+    let num = 0;
+    for (let i = 1; i <= limit; i++) {
+        num += i;
+    }
+}
+
+const getSomeCodeExecutionTime = () => {
+    const startTime = Date.now();
+    const randomNumFromDate = Date.now() % 10;
+
+    doSomething(randomNumFromDate * 1e5 * 7);
+
+    const endTime = Date.now();
+
+    const result = (endTime - startTime);
+    return result;
+}
+
+
+/******* Event Listner for generate button  *******/
+
+const goButton = document.querySelector('.go-button');
+const resultBox = document.querySelector('.result');
+
+goButton.addEventListener('click', (e) => {
+    resultBox.innerText = "?";
+    resultBox.style.backgroundColor = `hsl(0, 0%, 35%)`;
+
+    if (resultBox.classList.contains('rotation')) {
+        resultBox.classList.remove('rotation');
+        resultBox.classList.add('reverse-rotation');
+    }
+    else {
+        resultBox.classList.remove('reverse-rotation');
+        resultBox.classList.add('rotation');
+    }
+
+    goButton.classList.add('disable-click');
+
+    setTimeout(() => {
+        getNumbers();
+        goButton.classList.remove('disable-click');
+    }, 1000);
+
+});
+
+
+/******* Event Listner for Input of Upper Limit *******/
+
+let upperValue = document.querySelector('#upper-limit')
+
+document.querySelectorAll('.value-change-button').forEach(elem => elem.addEventListener("click", (e) => {
+    const type = e.target.getAttribute('data-type')
+
+    if (type === 'increment') {
+        upperValue.value++;
+    }
+    else if (type === 'decrement') {
+        upperValue.value--;
+    }
+})
+)
+
+
+/******* Function to get generate Random Number*******/
+
+let getNumbers = () => {
+
+    if (upperValue.value < 1) {
+        upperValue.value = 1;
+    } else if (upperValue.value > maxLimitForNumberGeneration) {
+        upperValue.value = maxLimitForNumberGeneration;
+    } else if (upperValue.value === '') {
+        upperValue.value = 10;
+    }
+
+    let date = Date.now();
+
+    let lowerLimit = 1;
+    let upperLimit = document.querySelector('#upper-limit').value;
+
+
+
+    let NoOfDigits = Math.ceil(Math.log10(upperLimit) + 0.001);
+
+    // Adjusting the font size according to number of digits
+    if (NoOfDigits >= 10) {
+        resultBox.style.fontSize = '1.5rem';
+    } else if (NoOfDigits >= 8) {
+        resultBox.style.fontSize = '2rem';
+    } else if (NoOfDigits >= 6) {
+        resultBox.style.fontSize = '2.75rem';
+    } else if (NoOfDigits >= 4) {
+        resultBox.style.fontSize = '3.75rem';
+    } else if (NoOfDigits < 4) {
+        resultBox.style.fontSize = '5rem';
+    }
+    
+    let SomeCodeExecutionTime = getSomeCodeExecutionTime();
+    // console.log((seed % 1e9), (date % 1e9), SomeCodeExecutionTime);
+
+    const randomNumber = ((seed % 1e9) * (date % 1e9)) % MAX_NUM + SomeCodeExecutionTime;
+    seed = randomNumber;
+
+
+    const result = (randomNumber % upperLimit) + lowerLimit;
+    // console.log(randomNumber, result)
+
+    const colorNum = (randomNumber % 10 + 1) * 36;
+
+    document.querySelector('#result').
+        innerText = result;
+    document.querySelector('#result').style.backgroundColor = `hsl(${colorNum % 360}, 100%, 35%)`;
+}
